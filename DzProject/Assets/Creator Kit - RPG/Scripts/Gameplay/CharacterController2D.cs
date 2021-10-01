0abf32a68dd4870eac89da83b4a702eb98445947
David Zetterdahl <david.zetterdahl@hiq.se>
2021-10-01 20:54:53 +0200

    5

---
+++ DzProject/Assets/Creator Kit - RPG/Scripts/Gameplay/CharacterController2D.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+
+namespace RPGM.Gameplay
+{
+    /// <summary>
+    /// A simple controller for animating a 4 directional sprite using Physics.
+    /// </summary>
+    public class CharacterController2D : MonoBehaviour
+    {
+        public float speed = 1;
+        public float acceleration = 2;
+        public float stepSize = 0.1f;
+        public float MinDistanceMovement = 0f;
+
+        public Vector3 nextMoveCommand;
+        public Animator animator;
+        public bool flipX = false;
+        private Vector3 desiredPos;
+
+        new Rigidbody2D rigidbody2D;
+        SpriteRenderer spriteRenderer;
+        PixelPerfectCamera pixelPerfectCamera;
+
+        enum State
+        {
+            Idle, Moving
+        }
+
+        State state = State.Idle;
+        Vector2 currentVelocity;
+
+        public void GoToPosition(Vector3 pos)
+        {
+            desiredPos = pos;
+        }
+
+        void IdleState()
+        {
+            if (nextMoveCommand != Vector3.zero)
+            {
+                UpdateAnimator(nextMoveCommand);
+                //nextMoveCommand = Vector3.zero;
+                state = State.Moving;
+            }
+        }
+
+        void MoveState()
+        {
+            UpdateAnimator(nextMoveCommand);
+            rigidbody2D.velocity = Vector2.SmoothDamp(rigidbody2D.velocity, nextMoveCommand * speed, ref currentVelocity, acceleration, speed);
+            spriteRenderer.flipX = rigidbody2D.velocity.x >= 0 ? true : false;
+        }
+
+        void UpdateAnimator(Vector3 direction)
+        {
+            if (animator)
+            {
+                animator.SetInteger("WalkX", direction.x < 0 ? -1 : direction.x > 0 ? 1 : 0);
+                animator.SetInteger("WalkY", direction.y < 0 ? 1 : direction.y > 0 ? -1 : 0);
+            }
+        }
+
+        void Update()
+        {
+            if (desiredPos != Vector3.zero)
+            {
+                Vector3 movement = Vector3.zero;
+                float distX = Mathf.Abs(transform.position.x - desiredPos.x);
+                float distY = Mathf.Abs(transform.position.y - desiredPos.y);
+
+                if (distX > MinDistanceMovement && Mathf.Abs(desiredPos.x) > 0)
+                {
+                    if (transform.position.x > desiredPos.x)
+                    {
+                        movement = Vector3.left;
+                    }
+                    else if (transform.position.x < desiredPos.x)
+                    {
+                        movement = Vector3.right;
+                    }
+                }
+
+                if (distY > MinDistanceMovement && Mathf.Abs(desiredPos.y) > 0)
+                {
+                    if (transform.position.y > desiredPos.y)
+                    {
+                        movement += Vector3.down;
+                    }
+                    else if (transform.position.y < desiredPos.y)
+                    {
+                        movement += Vector3.up;
+                    }
+                }
+
+                nextMoveCommand = movement * stepSize;
+            }
+
+            switch (state)
+            {
+                case State.Idle:
+                    IdleState();
+                    break;
+                case State.Moving:
+                    MoveState();
+                    break;
+            }
+
+        }
+
+        void LateUpdate()
+        {
+            if (pixelPerfectCamera != null)
+            {
+                transform.position = pixelPerfectCamera.RoundToPixel(transform.position);
+            }
+        }
+
+        void Awake()
+        {
+            rigidbody2D = GetComponent<Rigidbody2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            pixelPerfectCamera = GameObject.FindObjectOfType<PixelPerfectCamera>();
+        }
+    }
+}
---
+++ DzProject/Assets/Creator Kit - RPG/Scripts/Gameplay/MusicController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace RPGM.Gameplay
+{
+    public class MusicController : MonoBehaviour
+    {
+        public AudioMixerGroup audioMixerGroup;
+        public AudioClip audioClip;
+        public bool Muted = false;
+        public float crossFadeTime = 3;
+
+        AudioSource audioSourceA, audioSourceB;
+        float audioSourceAVolumeVelocity, audioSourceBVolumeVelocity;
+
+        public void CrossFade(AudioClip audioClip)
+        {
+            var t = audioSourceA;
+            audioSourceA = audioSourceB;
+            audioSourceB = t;
+            audioSourceA.clip = audioClip;
+            audioSourceA.Play();
+        }
+
+        void Update()
+        {
+            if (Muted)
+                return;
+
+            audioSourceA.volume = Mathf.SmoothDamp(audioSourceA.volume, 1f, ref audioSourceAVolumeVelocity, crossFadeTime, 1);
+            audioSourceB.volume = Mathf.SmoothDamp(audioSourceB.volume, 0f, ref audioSourceBVolumeVelocity, crossFadeTime, 1);
+
+        }
+
+        void OnEnable()
+        {
+            if (Muted)
+                return;
+
+            audioSourceA = gameObject.AddComponent<AudioSource>();
+            audioSourceA.spatialBlend = 0;
+            audioSourceA.clip = audioClip;
+            audioSourceA.loop = true;
+            audioSourceA.outputAudioMixerGroup = audioMixerGroup;
+            audioSourceA.Play();
+
+            audioSourceB = gameObject.AddComponent<AudioSource>();
+            audioSourceB.spatialBlend = 0;
+            audioSourceB.loop = true;
+            audioSourceB.outputAudioMixerGroup = audioMixerGroup;
+        }
+    }
+}
