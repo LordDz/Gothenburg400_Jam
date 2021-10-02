e7ec5aa5328fb9a8f60633fd91719db715ce787c
David Zetterdahl <david.zetterdahl@hiq.se>
2021-10-02 10:14:43 +0200

    Updated UI.

--- DzProject/Assets/_Game/Scripts/Player/MousePointer.cs
+++ DzProject/Assets/_Game/Scripts/Player/MousePointer.cs
@@ -1,18 +1,30 @@
 using RPGM.Gameplay;
 using UnityEngine;
 
+
+public enum MouseImgType
+{
+    Interact,
+    Walk,
+    Talk
+}
+
 public class MousePointer : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Texture2D CursorTexture;
+    public Texture2D CursorInteract;
+    public Texture2D CursorWalk;
+    public Texture2D CursorTalk;
     public AudioSource AudioClick;
 
     public Transform MouseClickImg;
     public CharacterController2D Player;
 
+    private bool isInUI = false;
+
     public void Start()
     {
-        Cursor.SetCursor(CursorTexture, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(CursorWalk, Vector2.zero, CursorMode.Auto);
     }
 
     void Update()
@@ -25,10 +37,41 @@ public class MousePointer : MonoBehaviour
 
     private void MouseClick()
     {
+        if (isInUI)
+        {
+            return;
+        }
         AudioClick.Play();
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
         MouseClickImg.position = mousePos;
         Player.GoToPosition(mousePos);
+    }
+
+    public void UIEnter()
+    {
+        isInUI = true;
+    }
+
+    public void UIExit()
+    {
+        isInUI = false;
+    }
+
+    public void SetCursorImg(MouseImgType mouseImgType)
+    {
+        switch (mouseImgType)
+        {
+            case MouseImgType.Interact:
+                Cursor.SetCursor(CursorInteract, Vector2.zero, CursorMode.Auto);
+                break;
+            case MouseImgType.Walk:
+                Cursor.SetCursor(CursorWalk, Vector2.zero, CursorMode.Auto);
+                break;
+            case MouseImgType.Talk:
+                Cursor.SetCursor(CursorTalk, Vector2.zero, CursorMode.Auto);
+                break;
+
+        }
     }
 
 }
---
+++ DzProject/Assets/_Game/Scripts/UI/InventoryMouseDetection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets._Game.Scripts.UI
+{
+    public class InventoryMouseDetection : MonoBehaviour
+     , IPointerEnterHandler
+     , IPointerExitHandler
+    {
+
+        public MousePointer mousePointer;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            mousePointer.UIEnter();
+            mousePointer.SetCursorImg(MouseImgType.Interact);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            mousePointer.UIExit();
+            mousePointer.SetCursorImg(MouseImgType.Walk);
+        }
+    }
+}
